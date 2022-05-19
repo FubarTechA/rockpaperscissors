dd8350b587d731dbae82ee8577f5ce025b056ed4
FubarTechA <tjfubar11@gmail.com>
2022-05-19 12:33:03 +0100

    new commit

--- script.js
+++ script.js
@@ -5,8 +5,16 @@ const close = document.querySelector(".close");
 const optionDiv = document.querySelector(".opt-div");
 const start = document.querySelector(".start");
 const game = document.querySelector(".game");
+const playerOption = document.querySelector(".player-opt");
+const computerOption = document.querySelector(".computer-opt");
+const playerOptionImg = playerOption.querySelector("img");
+const computerOptionImg = computerOption.querySelector("img");
 // console.log(ruleBtn);
 
+const computerhands = ["rock", "paper", "scissors"];
+let index = Math.floor(Math.random() * 3);
+
+let hand;
 ruleBtn.addEventListener("click", function () {
   overlay.classList.remove("hidden");
 });
@@ -23,4 +31,18 @@ optionDiv.addEventListener("click", function (e) {
   const option = e.target.closest(".option");
   start.classList.add("hidden");
   game.classList.remove("hidden");
+  let playerhand = option.dataset.name;
+  let src = `/images/icon-${playerhand}.svg`;
+  console.log(src);
+  playerOption.classList.add(playerhand);
+  playerOptionImg.setAttribute("src", src);
+  const compShowHand = function () {
+    let comphand = computerhands[index];
+    let compsrc = `/images/icon-${comphand}.svg`;
+    computerOption.classList.remove("comp-opt");
+    computerOption.classList.add("opt");
+    computerOption.classList.add(comphand);
+    computerOptionImg.setAttribute("src", compsrc);
+  };
+  setTimeout(compShowHand, 1000);
 });
